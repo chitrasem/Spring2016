19e219e7084c3869261fd59d887cf93339092ced
Sabaychitra84 <sabaychitra84@gmail.com>
2016-10-27 11:07:35 +0700

    kmtInitializer

--- kmt/src/main/java/com/chitra/kmt/configuration/KmtInitializer.java
+++ kmt/src/main/java/com/chitra/kmt/configuration/KmtInitializer.java
@@ -1,5 +1,30 @@
 package com.chitra.kmt.configuration;
 
-public class KmtInitializer {
+import javax.servlet.Filter;
 
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class KmtInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+ 
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { KmtConfiguration.class };
+    }
+  
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+  
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+    
+    @Override
+    protected Filter[] getServletFilters() {
+    	Filter [] singleton = { new CORSFilter()};
+    	return singleton;
+    }
+ 
 }
