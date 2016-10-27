c5ee0e8a7f186cbcf9c6052465ebebac25a8720c
Sabaychitra84 <sabaychitra84@gmail.com>
2016-10-27 11:06:29 +0700

    kmtConfig

--- kmt/src/main/java/com/chitra/kmt/configuration/KmtConfiguration.java
+++ kmt/src/main/java/com/chitra/kmt/configuration/KmtConfiguration.java
@@ -1,5 +1,13 @@
 package com.chitra.kmt.configuration;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.chitra.kmt")
 public class KmtConfiguration {
+	
 
 }
